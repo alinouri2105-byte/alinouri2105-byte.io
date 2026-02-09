14c6a78e05d2519fe26ce6fce30a564cc053cc6e
alinouri2105-byte <ali.nouri.2105@gmail.com>
2026-02-09 11:39:17 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    window.onload =
+      document.querySelector('.html').style.width = '90%';
+      document.querySelector('.css').style.width = '85%';
+      document.querySelector('.js').style.width = '70%';
+    </script>
